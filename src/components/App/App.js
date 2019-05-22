ca03035eb98c005641245f4aa1a436e13ad28bfa
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-05-22 14:41:22 +0430

    Add Business Component and render it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Business from '../Business/Business';
 
 function App() {
   return (
     <div className="App">
-      
+        <Business/>
     </div>
   );
 }
