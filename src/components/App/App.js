cd9180d0aaeed05fc0d21ac565d23b13c25a3ae9
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-05-22 22:05:04 +0430

    Add SearchBar and Render project and release v0.1.0

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Business from '../Business/Business';
-
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 function App() {
   return (
     <div className="App">
-        <Business/>
+      <h1>ravenous</h1>
+      <SearchBar/>
+      <BusinessList/>
     </div>
   );
 }
