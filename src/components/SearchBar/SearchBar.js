568d1ab50ad9b23fc7614f8f9abd27b36f540956
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-05-27 00:28:00 +0430

    Set apiKey and test and validation project V2.0.0

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,65 +1,67 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React,{Component} from 'react';
 import './SearchBar.css';
-
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
+//initial state
     this.state = {
       term: '',
       location: '',
       sortBy: 'best_match'
     };
-
+//bind methods
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.handleSortByChange = this.handleSortByChange.bind(this);
-
+//sort options
     this.sortByOptions = {
       'Best Match': 'best_match',
       'Highest Rated': 'rating',
       'Most Reviewed': 'review_count'
     };
   }
-
+// active class to sortOption
   getSortByClass(sortByOption) {
     if (this.state.sortBy === sortByOption) {
       return 'active';
     }
     return '';
   }
-
+//set sortBy
   handleSortByChange(sortByOption) {
     this.setState({sortBy: sortByOption});
   }
-
+//set term
   handleTermChange(event) {
     this.setState({term: event.target.value});
   }
-
+//set location
   handleLocationChange(event) {
     this.setState({location: event.target.value});
   }
-
+//send data to server and get response
   handleSearch(event) {
-    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
-
+    if(this.state.term && this.state.location && this.state.sortBy){
+      this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    }else{
+      alert('please fill all input')
+    }
     event.preventDefault();
   }
-
+//list of options to sorting
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
       return (<li className={this.getSortByClass(sortByOptionValue)}
                   key={sortByOptionValue}
-                  onClick={this.handleSortByChange}>
+                  onClick={() => this.handleSortByChange(sortByOptionValue)}>
                 {sortByOption}
              </li>);
     });
   }
-
+//render app
   render() {
     return (
       <div className="SearchBar">
--- src/util/Yalp.js
+++ src/util/Yalp.js
@@ -1,14 +1,20 @@
-const apiKey = ''; // Insert API key here.
-
+//your apiKey 
+const apiKey = 'Iv-7k_84izBflQykMMY92Kw-LHFpYiKCeKxGxifjaG5f7yEBGvrUZcMAxWtKCJlsMw1qbA9jTv4FGOJcSJawDMmllKHO6IP9WLCqTvdVMV7oLKy6aLlgVfCgKKLqXHYx'; // Insert API key here.
+//yelp module
 const Yelp = {
+//search data from api and get response
   search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
         Authorization: `Bearer ${apiKey}`
       }
-    }).then(response => {
+    })
+    //change response to json
+    .then(response => {
       return response.json();
-    }).then(jsonResponse => {
+    })
+    //get response and create businesses object
+    .then(jsonResponse => {
       if (jsonResponse.businesses) {
         return jsonResponse.businesses.map(business => ({
           id: business.id,
